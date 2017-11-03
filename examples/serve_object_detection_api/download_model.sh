b9ea11f89fde9b57e70c102248a5b5f4095762c7
dolaameng <dolabot@gmail.com>
2017-11-03 11:06:23 +0800

    create object detection api exmaple

---
+++ examples/serve_object_detection_api/download_model.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+PATH_TO_CKPT="http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_11_06_2017.tar.gz"
+PATH_TO_LABELS="https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/data/mscoco_label_map.pbtxt"
+
+
+
+wget -P ./models/ $PATH_TO_CKPT
+wget -P ./models/ $PATH_TO_LABELS
